def670e4fffdaae823c51e49ee1295bcfc376adb
GrizzlLearn <dylebedev.learning@gmail.com>
2024-08-01 00:31:24 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]: done

---
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oCityRepository.java
@@ -0,0 +1,30 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.dreamjob.model.City;
+
+import java.util.Collection;
+
+/**
+ * @author dl
+ * @date 31.07.2024 23:42
+ */
+
+@Repository
+public class Sql2oCityRepository implements CityRepository {
+
+	private final Sql2o sql2o;
+
+	public Sql2oCityRepository(Sql2o sql2o) {
+		this.sql2o = sql2o;
+	}
+
+	@Override
+	public Collection<City> findAll() {
+		try (var connection = sql2o.open()) {
+			var query = connection.createQuery("SELECT * FROM cities");
+			return query.executeAndFetch(City.class);
+		}
+	}
+}
